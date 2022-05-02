761c8ed54c802d4ddfc3fbea05f99d59ce3333ed
rkskekzzz <wkdlfflxh@naver.com>
2022-05-02 21:14:44 +0900

    Feat : week2

--- week2/script.js
+++ week2/script.js
@@ -40,23 +40,79 @@ const result = {
     explain: '하마 설명',
     img: 'lion.jpg',
   },
-  ISFJ: { animal: '부엉이', explain: '부엉이 설명', img: 'lion.jpg' },
-  INFJ: { animal: '물소', explain: '물소 설명', img: 'lion.jpg' },
-  INTJ: { animal: '치타', explain: '치타 설명', img: 'lion.jpg' },
-  ISTP: { animal: '나무늘보', explain: '나무늘보 설명', img: 'lion.jpg' },
-  ISFP: { animal: '거북이', explain: '거북이 설명', img: 'lion.jpg' },
-  INFP: { animal: '코끼리', explain: '코끼리 설명', img: 'lion.jpg' },
-  INTP: { animal: '침팬지', explain: '침팬지 설명', img: 'lion.jpg' },
-  ESTP: { animal: '악어', explain: '악어 설명', img: 'lion.jpg' },
-  ESFP: { animal: '미어캣', explain: '미어캣 설명', img: 'lion.jpg' },
-  ENFP: { animal: '멋쟁이 사자', explain: '멋쟁이 사자 설명', img: 'lion.jpg' },
-  ENTP: { animal: '태양새', explain: '태양새 설명', img: 'lion.jpg' },
-  ESTJ: { animal: '기린', explain: '기린 설명', img: 'lion.jpg' },
-  ESFJ: { animal: '고릴라', explain: '고릴라 설명', img: 'lion.jpg' },
-  ENFJ: { animal: '카피바라', explain: '카피바라 설명', img: 'lion.jpg' },
-  ENTJ: { animal: '호랑이', explain: '호랑이 설명', img: 'lion.jpg' },
+  ISFJ: { animal: '부엉이', explain: '부엉이 설명', img: 'lion.png' },
+  INFJ: { animal: '물소', explain: '물소 설명', img: 'lion.png' },
+  INTJ: { animal: '치타', explain: '치타 설명', img: 'lion.png' },
+  ISTP: { animal: '나무늘보', explain: '나무늘보 설명', img: 'lion.png' },
+  ISFP: { animal: '거북이', explain: '거북이 설명', img: 'lion.png' },
+  INFP: { animal: '코끼리', explain: '코끼리 설명', img: 'lion.png' },
+  INTP: { animal: '침팬지', explain: '침팬지 설명', img: 'lion.png' },
+  ESTP: { animal: '악어', explain: '악어 설명', img: 'lion.png' },
+  ESFP: { animal: '미어캣', explain: '미어캣 설명', img: 'lion.png' },
+  ENFP: { animal: '멋쟁이 사자', explain: '멋쟁이 사자 설명', img: 'lion.png' },
+  ENTP: { animal: '태양새', explain: '태양새 설명', img: 'lion.png' },
+  ESTJ: { animal: '기린', explain: '기린 설명', img: 'lion.png' },
+  ESFJ: { animal: '고릴라', explain: '고릴라 설명', img: 'lion.png' },
+  ENFJ: { animal: '카피바라', explain: '카피바라 설명', img: 'lion.png' },
+  ENTJ: { animal: '호랑이', explain: '호랑이 설명', img: 'lion.png' },
 };
 
 let num = 1;
 
 // 여기까지만 사전 제공
+titleBtn.addEventListener('click', () => {
+  titleContainer.style.display = 'none';
+  questionContainer.style.display = 'block';
+  updateQuestion();
+});
+
+aBtn.addEventListener('click', () => {
+  switch (type.innerHTML) {
+    case 'EI':
+      let e = parseInt(EI.value);
+      EI.setAttribute('value', e + 1);
+      break;
+    case 'SN':
+      let s = parseInt(SN.value);
+      SN.setAttribute('value', s + 1);
+      break;
+    case 'TF':
+      let t = parseInt(SN.value);
+      SN.setAttribute('value', t + 1);
+      break;
+    case 'JP':
+      let j = parseInt(SN.value);
+      SN.setAttribute('value', j + 1);
+      break;
+  }
+  updateQuestion();
+});
+
+bBtn.addEventListener('click', () => {
+  updateQuestion();
+});
+
+function updateQuestion() {
+  if (num == 13) {
+    questionContainer.style.display = 'none';
+    resultContainer.style.display = 'block';
+
+    let mbti = '';
+
+    EI.value > 2 ? (mbti += 'E') : (mbti += 'I');
+    SN.value > 2 ? (mbti += 'S') : (mbti += 'N');
+    TF.value > 2 ? (mbti += 'T') : (mbti += 'F');
+    JP.value > 2 ? (mbti += 'J') : (mbti += 'P');
+
+    MBTI.innerHTML = mbti;
+    explain.innerHTML = result[mbti].explain;
+    image.setAttribute('src', result[mbti].img);
+  } else {
+    pro.setAttribute('style', `width: calc(100/12*${num}%)`);
+    question.innerHTML = q[num].title;
+    type.innerHTML = q[num].type;
+    aBtn.innerHTML = q[num].A;
+    bBtn.innerHTML = q[num].B;
+    num++;
+  }
+}
