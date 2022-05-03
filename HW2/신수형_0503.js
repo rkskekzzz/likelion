4332b31c046f09b69a72eb2e8f3d6c03b345ca7e
rkskekzzz <wkdlfflxh@naver.com>
2022-05-03 16:11:48 +0900

    Feat : 과제 2주차

---
+++ HW2/신수형_0503.js
@@ -0,0 +1,26 @@
+// 문제 1
+let object = { name: 'likelion', color: 'yellow' };
+
+console.log(object);
+
+// 문제 2
+let likelion = {
+  1: { name: '김대연', mbti: 'ENFJ' },
+  2: { name: '김지혜', mbti: 'INTJ' },
+  3: { name: '조나원', mbti: 'ENFP' },
+};
+
+console.log(likelion[3]);
+console.log(likelion[1]['name']); // 김대연
+
+// 문제 3
+// first()
+// text()
+// val()
+// html()
+
+// 문제 4
+// elemtext.style.textAlign = "center";
+
+// 문제 4
+// 1-2, 2-2
