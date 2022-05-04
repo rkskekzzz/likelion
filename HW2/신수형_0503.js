d8c0dfcaa8d13459d013d24a5a36cd76d9a6959c
rkskekzzz <wkdlfflxh@naver.com>
2022-05-04 16:08:11 +0900

    수정

--- HW2/신수형_0503.js
+++ HW2/신수형_0503.js
@@ -2,6 +2,7 @@
 let object = { name: 'likelion', color: 'yellow' };
 
 console.log(object);
+// 출력 결과 : { name: 'likelion', color: 'yellow' }
 
 // 문제 2
 let likelion = {
@@ -10,14 +11,14 @@ let likelion = {
   3: { name: '조나원', mbti: 'ENFP' },
 };
 
-console.log(likelion[3]);
+console.log(likelion[3]['mbti']);
 console.log(likelion[1]['name']); // 김대연
 
 // 문제 3
 // first()
 // text()
-// val()
-// html()
+// val(parseInt(text))
+// attr()
 
 // 문제 4
 // elemtext.style.textAlign = "center";
